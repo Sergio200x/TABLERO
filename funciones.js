36c533105810e831eb4b1902501cb48a54d52a12
Sergio Fernandez <sergio.fernandez@bgh.com.ar>
2021-10-21 16:22:11 -0300

    agregue validacion del usuario2 a la funcion transferirSaldo

--- funciones.js
+++ funciones.js
@@ -32,23 +32,29 @@ function consultarUser(usuario,password)
 }
 
 function transferirSaldo(usuario1,password,saldoAtransferir,usuario2)
-{      
+{      let buscarUsuario2=usuarios.filter(parametro=>parametro.nombreUsuario===usuario2)
         if(validarUsuario(usuario1,password)===usuario1)
-    {
-        let buscarUsuario1=consultarUser(usuario1,password)
-        let buscarUsuario2=usuarios.filter(parametro=>parametro.nombreUsuario===usuario2)
-
-        if((consultarUser(usuario1,password)[0].saldo-saldoAtransferir)>=0)
-        {
-            buscarUsuario1[0].saldo = buscarUsuario1[0].saldo-saldoAtransferir 
-            buscarUsuario2[0].saldo= buscarUsuario2[0].saldo+saldoAtransferir
-            console.log("Se ha descontado $"+saldoAtransferir+" de su cuenta y se a transferido a "+usuario1
-            +" ,su saldo actual es $"+buscarUsuario1[0].saldo)
-        }
-        else
-        {
-            console.log("Usted no tiene saldo suficiente para realizar esta transferencia")
-        }              
+    {       
+            if(buscarUsuario2.length>0)
+            {       
+                let buscarUsuario1=consultarUser(usuario1,password)               
+                if((consultarUser(usuario1,password)[0].saldo-saldoAtransferir)>=0)
+                {
+                    buscarUsuario1[0].saldo = buscarUsuario1[0].saldo-saldoAtransferir 
+                    buscarUsuario2[0].saldo= buscarUsuario2[0].saldo+saldoAtransferir
+                     console.log("Se ha descontado $"+saldoAtransferir+" de su cuenta y se a transferido a "+usuario1
+                    +" ,su saldo actual es $"+buscarUsuario1[0].saldo)
+                    
+                }
+                else
+                {
+                     console.log("Usted no tiene saldo suficiente para realizar esta transferencia")
+                 } 
+            }
+            else    
+            {
+                console.log("No existe el usuario al que desea transferir dinero")
+            }                  
     }
     else
     {
